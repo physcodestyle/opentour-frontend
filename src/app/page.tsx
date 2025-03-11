7164afc5a145770bc09521cfe9dc3ac64a8204b6
Волчугов Антон <toni.volchugov@mail.ru>
2025-03-11 16:55:39 +0300

    fix: переделал импорты

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import styles from './page.module.css'
+import './page.css'
 
 export default function Home() {
 	return (
-		<main className={styles.main}>
+		<main className='main'>
 			<h1>Header 1 readable text</h1>
 			<h2>Header 2 readable text</h2>
 			<h3>Header 3 readable text</h3>
