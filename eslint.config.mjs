97f4dd255d7f5b50f706f7fba4854d86d675acb1
Волчугов Антон <toni.volchugov@mail.ru>
2025-03-12 23:45:41 +0300

    fix: файл не доработан

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,50 @@
 import pluginJs from '@eslint/js'
-import pluginReact from 'eslint-plugin-react'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{
-		ignores: ['node_modules'],
-	},
-	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-	{
-		languageOptions: {
-			globals: {
-				...globals.node,
-				...globals.browser,
-				...globals.es2024,
-			},
-		},
-	},
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	pluginReact.configs.flat.recommended,
+  {
+    ignores: ['node_modules', '/.gitignore'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    settings: {
+      react: {
+        version: 'detected',
+      },
+    },
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2024,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+
+  ...pluginJs.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+    },
+  },
+
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+
+  /* pluginReact.configs.flat.recommended, */
 ]
