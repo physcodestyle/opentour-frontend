8a4d89038cf6a0de760561eb5cab2c8778ed59f3
Волчугов Антон <toni.volchugov@mail.ru>
2025-03-12 23:42:35 +0300

    fix: ушла ошибка EsLint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import './page.css'
 
 export default function Home() {
-	return (
-		<main className='main'>
-			<h1>Header 1 readable text</h1>
-			<h2>Header 2 readable text</h2>
-			<h3>Header 3 readable text</h3>
-		</main>
-	)
+  return (
+    <main className='main'>
+      <h1>Header 1 readable text</h1>
+      <h2>Header 2 readable text</h2>
+      <h3>Header 3 readable text</h3>
+    </main>
+  )
 }
