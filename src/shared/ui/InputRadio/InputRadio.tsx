495cc6849d1d112fa282b738681247099716e7f6
Kudina Ekaterina <lesurita@gmail.com>
2025-03-13 20:01:36 +0300

    feat: добавила стартовый файл Input-radio

---
+++ src/shared/ui/InputRadio/InputRadio.tsx
@@ -0,0 +1,10 @@
+import './Radio.css'
+
+export const InputRadio = () => {
+  return (
+    <div>
+      <input type='radio' id='huey' name='drone' value='huey' checked />
+      <label htmlFor='huey'>Huey</label>
+    </div>
+  )
+}
