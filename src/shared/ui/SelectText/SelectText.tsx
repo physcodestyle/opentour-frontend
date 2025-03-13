f306bd6f29341c22942d46ca13b92e1539cc45e8
Волчугов Антон <toni.volchugov@mail.ru>
2025-03-13 19:11:25 +0300

    feat: создал файл со стартовым компонентом select

---
+++ src/shared/ui/SelectText/SelectText.tsx
@@ -0,0 +1,10 @@
+import './SelectText.css'
+
+export const SelectText = () => {
+  return (
+    <select>
+      <option value='someOption'>Some option</option>
+      <option value='otherOption'>Other option</option>
+    </select>
+  )
+}
