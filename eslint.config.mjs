ad757ee107d8503ecadde6de09aea637e7132a19
Igor Korovchenko <igsekor@gmail.com>
2025-03-25 15:33:21 +0300

    fix: исправляет конфигурацию для ESLint 9

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,65 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import { fileURLToPath } from 'url';
+import * as path from 'path';
+
+import { fixupPluginRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import { globalIgnores } from 'eslint/config'
+import eslintJs from '@eslint/js';
+import eslintTs from 'typescript-eslint';
+import eslintReact from "eslint-plugin-react";
 
 
-export default [
-	{files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-	{languageOptions: { globals: globals.browser }},
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	pluginReact.configs.flat.recommended,
-];
+const project = "./tsconfig.json";
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const compat = new FlatCompat({
+    baseDirectory: dirname,
+    recommendedConfig: eslintJs.configs.recommended,
+});
+
+function legacyPlugin(name, alias = name) {
+    const plugin = compat.plugins(name)[0]?.plugins?.[alias];
+
+    if (!plugin) {
+        throw new Error(`Unable to resolve plugin ${name} and/or alias ${alias}`);
+    }
+
+    return fixupPluginRules(plugin);
+}
+
+export default eslintTs.config(
+    eslintJs.configs.recommended,
+    ...eslintTs.configs.recommended,
+    ...compat.extends("plugin:import/typescript"),
+    {
+        languageOptions: {
+            parserOptions: {
+                project,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        settings: {
+            "import/resolver": {
+                typescript: {
+                    alwaysTryTypes: true,
+                    project,
+                }
+            },
+        },
+        plugins: {
+            react: eslintReact,
+            import: legacyPlugin("eslint-plugin-import", "import"),
+        },
+    },
+    {
+        rules: {
+            "semi": 2,
+            "eqeqeq": 2,
+            "no-console": 2,
+            "react/jsx-first-prop-new-line": [2, 'multiline'],
+            "import/order": [2, { "newlines-between": "always", }],
+            "import/newline-after-import": [2, { "count": 2, }],
+        },
+    },
+		globalIgnores(['.next', '**/*.config.js', '**/*.config.mjs']),
+);
